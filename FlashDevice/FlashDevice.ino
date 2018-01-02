f5698ff8e9dcf2d1943af6f1cdbbb15b2243b69e
Joost MF Liebregts <contact@joostliebregts.nl>
2018-01-02 16:18:35 +0100

    added Findmode
    added Findmode

--- FlashDevice/FlashDevice.ino
+++ FlashDevice/FlashDevice.ino
@@ -1,35 +1,38 @@
-// the setup routine runs once when you press reset:
-int sensorValue = 0;
-int oldSensorValue = 0;
-float voltage = 0;
-float oldVoltage = 0;
-boolean button = true;
-boolean start = true;
-int p = 0;
-int flashActivated = false;
-int counter = 0;
-int lowestV = 0;
-int highestV = 0;
-int delayTime = 1;
-int throwDelay = 2; //Delay after releasing button in seconds. 
-float SRD = 0.33; // Delay between blinks during find mode in seconds
-long previousMillis = 0;        // will store last time LED was updated
-
-long interval = 100;           // interval at which to blink (milliseconds)
-
-byte flashingCounter = 0;
-
+#include <elapsedMillis.h>
+ //DEFINING PINS || DO NOT CHANGE
 const int LEDS = 0;
-const int FET = 2;
 const int BUTTONLED = 1;
+const int FET = 2;
 const int BUTTONVALUE = 4;
+
+//TIMERS
+long flashFrequency = 1000/30;     // interval at which to blink (milliseconds)
+long flashTime = 3; // Total time the flash flashes in seconds
+float delayTime = 1;
+float throwDelay = 2; //Delay after releasing button in seconds. 
+float SRD = 0.33; // Delay between blinks during find mode in seconds
+
+//INTERNAL STATES || DO NOT TOUCH
+boolean button = true; // INTERNAL BOOLEAN FOR TELLING THE STATE OF BUTTON
+boolean start = true; // WHETHER START LOOP IS ACTIVE OR NOT
+boolean flashActivated = false;  // STATE OF FLASH: FLASHING OR NOT FLASHING
+int sensorValue = 0; // BUTTON STATE INPUT
+int counter = 0; // COUNTER FOR COUNTING TIME BUTTON IS PRESSED
+int ledState = LOW;             // ledState used to set the LED
+long previousMillis = 0; // will store last time LED was updated
+long previousMillisFindMode = 0;    // will store last time LED was updated
+long previousMillisElapsedTime = 0; // will store last time LED was updated
+elapsedMillis timeElapsed;
+unsigned long flashElapsed;
+boolean initialFlash = true;
+boolean findModeActive = false; 
 
 void setup() {
   start = true;
   // initialize the digital pin as an output.
   pinMode(LEDS, OUTPUT); //LEDs
   pinMode(FET, OUTPUT); // Power FET
-  pinMode(BUTTONLED, OUTPUT); // Power Button LED
+  pinMode(BUTTONLED, OUTPUT); // Puwer Button LED
   pinMode(BUTTONVALUE, INPUT);
 }
 
@@ -38,7 +41,16 @@ void loop() {
   if (start == true) {
     firstBoot();
   }
-
+  if (flashActivated == true && button == false) {
+    if(initialFlash == true){
+      flashElapsed = timeElapsed;
+    }
+    initialFlash = false;
+    flash();
+  }
+  if (findModeActive == true){
+    findMode();
+    }
   checkButton();
   checkCounter();
 }
@@ -57,62 +69,69 @@ void firstBoot() {
 }
 
 void flash() {
-  delay(throwDelay * 1000);
-  for (int i = 0; i < 50; i++) {
-    digitalWrite(LEDS, HIGH);
-    delay(1000 / 30);
-    digitalWrite(LEDS, LOW);
-    delay(1000 / 30);
+  
+  unsigned long currentMillisElapsedTime = millis();
+  unsigned long currentMillis = millis();
+
+  if (timeElapsed - flashElapsed >= throwDelay * 1000 ){
+    if (currentMillis - previousMillis >= flashFrequency)
+    {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;
+      // if the LED is off turn it on and vice-versa:
+      if (ledState == LOW){
+        ledState = HIGH;
+      }else{
+        ledState = LOW;
+      }
+      // set the LED with the ledState of the variable:
+      digitalWrite(LEDS, ledState);
+    }
   }
-  findMode();
+
+  if(millis() - flashElapsed >= flashTime * 1000 + throwDelay * 1000){
+    ledState = LOW;
+    digitalWrite(LEDS, LOW);
+    previousMillisElapsedTime = currentMillisElapsedTime;
+    flashActivated = false;
+    findModeActive = true;
+  }
+  // for (int i = 0; i < 50; i++) {
+  //   digitalWrite(LEDS, HIGH);
+  //   delay(1000 / 30);
+  //   digitalWrite(LEDS, LOW);
+  //   delay(1000 / 30);
+  // }
+  // findMode();
 }
 
 //Variable timer function -> Set boolean to false after timer runs out. Non blocking code. (Button to cancel)
 void findMode() {
-  for (int i = 0; i < 5; i++) {
-    digitalWrite(BUTTONLED, HIGH);
-    delay(SRD * 1000);
-    digitalWrite(BUTTONLED, LOW);
-    delay(SRD * 1000);
+  if (millis() - previousMillisFindMode >= 500){
+    previousMillisFindMode = millis();
+    if (ledState == LOW){
+      ledState = HIGH;
+    }
+    else{
+      ledState = LOW;
+    }
+    // set the LED with the ledState of the variable:
+    digitalWrite(BUTTONLED, ledState);
   }
-  digitalWrite(FET, LOW);
-  delay(2000);
-  checkButton();
 }
 
 void checkButton() {
-
   sensorValue = digitalRead(BUTTONVALUE);
-
   if(sensorValue == HIGH){
-    digitalWrite(BUTTONLED, HIGH);
     if (start == true) {
       start = false;
     }
   }else{
-    digitalWrite(BUTTONLED, LOW);
   }
-
-  
-  //    unsigned long currentMillis = millis();
-//  sensorValue = analogRead(2); //Read p3
-//  if (sensorValue < oldSensorValue - 50) {
-//    oldSensorValue = sensorValue;
-//    lowestV = sensorValue;
-//  } else if (sensorValue > oldSensorValue + 50) {
-//    oldSensorValue = sensorValue;
-//    highestV = sensorValue;
-//    if (start == true) {
-//      start = false;
-//      //      flash();
-//    }
-//  }
-
 
   //  if (currentMillis - previousMillis > interval) {
   //    // save the last time you blinked the LED
   //    previousMillis = currentMillis;
-
   if (sensorValue == HIGH ) {
     if (flashActivated != true) {
       digitalWrite(BUTTONLED, HIGH);
@@ -123,8 +142,15 @@ void checkButton() {
       checkCounter();
       counter++;
     }
+    if(findModeActive == true){
+      digitalWrite(BUTTONLED, HIGH);
+      button = true;
+      findModeActive = false;
+      digitalWrite(FET, LOW);
+      delay(2000);
+    }
 
-  } else if (sensorValue == LOW) {
+  } else if (sensorValue == LOW && findModeActive == false) {
     digitalWrite(BUTTONLED, LOW);
     button = false;
     counter = 0;
@@ -140,8 +166,4 @@ void checkCounter() {
     digitalWrite(BUTTONLED, HIGH);
     flashActivated = true;
   }
-  if (flashActivated == true && button == false) {
-    flash();
-  }
-
 }
